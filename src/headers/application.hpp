659fa7af6cf0484907a7cb1cd51d064d2c07101b
McBrainless <jjbrainless@protonmail.com>
2021-11-20 20:57:44 -0600

    points & curves

--- src/headers/application.hpp
+++ src/headers/application.hpp
@@ -8,7 +8,8 @@
 #include <cstring>
 #include <vector>
 
-#include "./classes/point3d.hpp"
+#include "./point3d.hpp"
+#include "./curves.hpp"
 
 using namespace std;
 
@@ -31,9 +32,11 @@ enum loading_t {
 
 class application {
 		loading_t state;
-		vector <point3d> points;
+		vector <point3d*> points;
+		vector <curve3d*> curves;
 	public:
 		application();
+		~application();
 		void load_msh(string*);
 };
 
